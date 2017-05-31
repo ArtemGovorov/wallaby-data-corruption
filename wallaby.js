7585ddd798a6251af14092324e125c73625838c9
Ivan Zub <ivan.zub@mendix.com>
2017-05-31 16:40:58 +0200

    Initial commit

---
+++ wallaby.js
@@ -0,0 +1,24 @@
+module.exports = function (wallaby) {
+    return {
+        files: [
+            { pattern: './node_modules/**/*.*', instrument: false, load: false, ignore: true },
+            { pattern: './**/*.spec.+(ts|tsx)', instrument: false, load: false, ignore: true },
+            { pattern: './**/*.test.+(ts|tsx)', instrument: false, load: false, ignore: true },
+
+            { pattern: './files/**/*.*', instrument: false, load: false, ignore: false },
+            { pattern: './**/*.snap', instrument: false, load: false, ignore: false },
+            { pattern: './**/*.+(ts|tsx)', instrument: true, load: false, ignore: false },
+            "package.json",
+            "tsconfig.json"
+        ],
+        tests: [
+            '/**/*.spec.+(ts|tsx)',
+        ],
+        env: {
+            type: 'node',
+            runner: 'node',
+        },
+        testFramework: 'jest',
+        debug: true
+    }
+};
